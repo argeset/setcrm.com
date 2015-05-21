cba9a6446e376a3d9084729c8699bfe53681b94e
Salih Angün <salih_angun@hotmail.com>
2015-05-21 17:25:19 +0200

    setcrm contentpages

---
+++ src/Client/Controllers/ContentController.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+
+namespace Client.Controllers
+{
+    public class ContentController:BaseController
+    {
+        [HttpGet]
+        public ViewResult Customers()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult PrivacyAgreement()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult TermsofUse()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/Client/Controllers/PropertyController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+using Client.Models;
+
+namespace Client.Controllers
+{
+    public class PropertyController:BaseController
+    {
+        [HttpGet]
+        public ViewResult Index()
+        {
+            return View();
+        }
+         
+    }
+}
